9c94918aaddebf715deda60abfde9e2c938765b6
leehy <leeh2@yonsei.ac.kr>
2025-10-24 22:30:51 +0900

    STD

---
+++ android/app/src/main/kotlin/com/amaze/test_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.amaze.test_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.star),
+            Icon(Icons.ac_unit)
+          ]
+        )
+      )
+    ); // MaterialApp
+  }
+}
+
+
+
+
+
+
+
+
+
+
